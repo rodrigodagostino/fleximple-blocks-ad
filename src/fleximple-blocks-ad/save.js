9266b28464e82f85a8e2cc1d1b91a9cc1848ce82
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2026-05-27 01:28:08 -0300

    fix: render block only if media is set

--- src/fleximple-blocks-ad/save.js
+++ src/fleximple-blocks-ad/save.js
@@ -13,61 +13,65 @@ function AdSave({
 	const defaultClassName = getBlockDefaultClassName(name);
 
 	return (
-		<div className={defaultClassName} {...useBlockProps.save()} data-block-id={blockId}>
-			{linkUrl && (
-				<a
-					className={`${defaultClassName}__link`}
-					href={linkUrl}
-					target={linkTarget}
-					rel="noopener"
-					aria-label={linkLabel}
-				></a>
+		<>
+			{(url.small || url.medium || url.large) && (
+				<div className={defaultClassName} {...useBlockProps.save()} data-block-id={blockId}>
+					{linkUrl && (
+						<a
+							className={`${defaultClassName}__link`}
+							href={linkUrl}
+							target={linkTarget}
+							rel="noopener"
+							aria-label={linkLabel}
+						></a>
+					)}
+					{Object.entries(type).map(([key, value], index) => {
+						if (!value) return null;
+
+						const breakpointKeys = Object.keys(type);
+						const breakpointValues = Object.values(type);
+						const afterNextBreakpoint = breakpointKeys[index + 2];
+						const hasNextBreakpoint = breakpointValues[index + 1];
+						const hasAfterNextBreakpoint = breakpointValues[index + 2];
+						const hasLargerBreakpoint = Boolean(hasNextBreakpoint || hasAfterNextBreakpoint);
+
+						const getElementClasses = (element) =>
+							[
+								`${defaultClassName}__${element}`,
+								...(hasLargerBreakpoint && hasNextBreakpoint ? [`${key}-only`] : []),
+								...(hasLargerBreakpoint && !hasNextBreakpoint && hasAfterNextBreakpoint
+									? [`${key}-until-${afterNextBreakpoint}`]
+									: []),
+								...(!hasLargerBreakpoint ? [`${key}-and-above`] : []),
+							].join(' ');
+
+						if (value === 'image') {
+							return (
+								<picture key={key} className={getElementClasses('picture')}>
+									{!!id[key] && (
+										<img className={`${defaultClassName}__image`} src={url[key]} alt={alt} />
+									)}
+								</picture>
+							);
+						}
+
+						return (
+							<video
+								key={key}
+								className={getElementClasses('video')}
+								type={`video/${subtype[key]}`}
+								autoPlay
+								loop
+								muted
+							>
+								<source src={url[key]} />
+							</video>
+						);
+					})}
+					<InlineStyles {...{ defaultClassName, attributes }} />
+				</div>
 			)}
-			{Object.entries(type).map(([key, value], index) => {
-				if (!value) return null;
-
-				const breakpointKeys = Object.keys(type);
-				const breakpointValues = Object.values(type);
-				const afterNextBreakpoint = breakpointKeys[index + 2];
-				const hasNextBreakpoint = breakpointValues[index + 1];
-				const hasAfterNextBreakpoint = breakpointValues[index + 2];
-				const hasLargerBreakpoint = Boolean(hasNextBreakpoint || hasAfterNextBreakpoint);
-
-				const getElementClasses = (element) =>
-					[
-						`${defaultClassName}__${element}`,
-						...(hasLargerBreakpoint && hasNextBreakpoint ? [`${key}-only`] : []),
-						...(hasLargerBreakpoint && !hasNextBreakpoint && hasAfterNextBreakpoint
-							? [`${key}-until-${afterNextBreakpoint}`]
-							: []),
-						...(!hasLargerBreakpoint ? [`${key}-and-above`] : []),
-					].join(' ');
-
-				if (value === 'image') {
-					return (
-						<picture key={key} className={getElementClasses('picture')}>
-							{!!id[key] && (
-								<img className={`${defaultClassName}__image`} src={url[key]} alt={alt} />
-							)}
-						</picture>
-					);
-				}
-
-				return (
-					<video
-						key={key}
-						className={getElementClasses('video')}
-						type={`video/${subtype[key]}`}
-						autoPlay
-						loop
-						muted
-					>
-						<source src={url[key]} />
-					</video>
-				);
-			})}
-			<InlineStyles {...{ defaultClassName, attributes }} />
-		</div>
+		</>
 	);
 }
 
